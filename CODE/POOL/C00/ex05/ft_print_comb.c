9f378670805baf30e2a1f2a71672249ebd6db37c
Mr-Crazy8 <anaselmendili13@gmail.com>
2024-01-10 19:08:46 +0100

    work

---
+++ CODE/POOL/C00/ex05/ft_print_comb.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb(void)
+{
+	int d1, d2, d3;
+
+	d1 = '0';
+	while (d1 <= '7')
+	{
+		d2 = d1 + 1;
+		while (d2 <= '8')
+		{
+			d3 = d2 + 1;
+			while (d3 <= '9')
+			{
+				ft_putchar(d1);
+				ft_putchar(d2);
+				ft_putchar(d3);
+				if (!(d1 == '7' && d2 == '8' && d3 == '9'))
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				d3++;
+			}
+			d2++;
+		}
+		d1++;
+	}
+}
